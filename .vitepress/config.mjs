f8f7f9191188f2e42018133cd1c78c926e1ec575
yym-yumeng123 <18026493873@163.com>
2023-12-13 11:13:21 +0800

    use vitepress

---
+++ .vitepress/config.mjs
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "学海无涯",
+  description: "吾日三省吾身",
+  srcDir: "src",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    // 设置导航栏
+    // nav: [
+    //   { text: 'Home', link: '/' },
+    //   { text: 'Examples', link: '/markdown-examples' }
+    // ],
+
+    docFooter: { prev: '上一篇', next: '下一篇' },
+
+    sidebar: [
+      {
+        text: "快速阅读",
+        link: '/started/started'
+      },
+      {
+        text: '前端',
+        items: [
+          { text: 'Html知识点', link: '/html/html' },
+          { text: 'Css知识点', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/yym-yumeng123/InterviewExperience' }
+    ]
+  }
+})
---
+++ src/index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "面经"
+  text: "巩固复习学习知识"
+  # tagline: My great project tagline
+  actions:
+    - theme: brand
+      text: 开始学习
+      link: /html/html
+    # - theme: alt
+    #   text: API Examples
+    #   link: /api-examples
+
+features:
+  - title: JavaScript
+    details: 巩固前端基础知识点
+  - title: Golang
+    details: 学习Golang
+  - title: tools
+    details: 各种工具的使用
+---
+
